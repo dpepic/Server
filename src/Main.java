ce90d8e1f03f489fa32e6061551c58802f13e2ab
Dragan <rechiefy@gmail.com>
2019-02-27 11:04:04 +0100

    Threading

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.net.*;
 import java.io.*;
 
-
 public class Main 
 {
 	public static void main(String[] args) 
@@ -12,26 +11,47 @@ public class Main
 			while(true)
 			{
 				System.out.println("Cekam konekciju...");
-				Socket klijent = soket.accept();
-				System.out.println("Konekcija sa: " + klijent.getInetAddress());
+				Konekcija nekaKonekcija = new Konekcija(soket.accept());
+				nekaKonekcija.run();
+			}
+		} catch (IOException joj)
+		{
+			joj.printStackTrace();
+		}
+	}
+}
 
-				OutputStream slanje = klijent.getOutputStream();
-				OutputStreamWriter upisivac = new OutputStreamWriter(slanje);
-				BufferedWriter bUpisivac = new BufferedWriter(upisivac);
+class Konekcija
+{
+	Socket klijent; 
+	
+	public Konekcija(Socket koSeKonektuje)
+	{
+		this.klijent = koSeKonektuje;
+	}
 
-				InputStream primanje = klijent.getInputStream();
-				InputStreamReader citac = new InputStreamReader(primanje);
-				BufferedReader bCitac = new BufferedReader(citac);
+	public void run()
+	{
+		try
+		{
+			System.out.println("Konekcija sa: " + klijent.getInetAddress());
 
-				String ulaz;
+			OutputStream slanje = klijent.getOutputStream();
+			OutputStreamWriter upisivac = new OutputStreamWriter(slanje);
+			BufferedWriter bUpisivac = new BufferedWriter(upisivac);
 
-				while ((ulaz = bCitac.readLine()) != null)
-				{
-					ulaz = ulaz.toUpperCase(); 
-					bUpisivac.write("Server kaze: " + ulaz);
-					bUpisivac.newLine();
-					bUpisivac.flush();
-				}
+			InputStream primanje = klijent.getInputStream();
+			InputStreamReader citac = new InputStreamReader(primanje);
+			BufferedReader bCitac = new BufferedReader(citac);
+
+			String ulaz;
+
+			while ((ulaz = bCitac.readLine()) != null)
+			{
+				ulaz = ulaz.toUpperCase(); 
+				bUpisivac.write("Server kaze: " + ulaz);
+				bUpisivac.newLine();
+				bUpisivac.flush();
 			}
 		} catch (IOException joj)
 		{
